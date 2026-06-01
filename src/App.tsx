0c6c63af669e899808338e905e7ef37830d511fd
Gershon <gwhitlock@perseverenow.org>
2026-06-01 18:57:39 -0500

    chore: initialize Vite React TypeScript project and add Header component

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header/Header";
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+      <h1>hello world</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import "./Header.css";
+
+const Header = () => {
+  return <div className="header">Header is in the house </div>;
+};
+
+export default Header;
